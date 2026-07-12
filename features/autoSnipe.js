4a91a06a196a040f96f04545943bf03be1892a6b
Conoutlet <benjaoq@gmail.com>
2026-07-11 23:51:50 -0600

    refactor: clean up embed creation and improve code formatting in auto snipe feature

--- features/autoSnipe.js
+++ features/autoSnipe.js
@@ -1,21 +1,22 @@
 const { EmbedBuilder } = require('discord.js');
 const mensajesEliminados = require('../utils/store.js');
 
-module.exports = {
-  execute: (message, authorData) => {
-    const embed = new EmbedBuilder()
-      .setColor(authorData.color)
-      .setAuthor({
-        name: `${authorData.nombre} (ID: ${authorData.id})`,
-        iconURL: authorData.avatar,
-      })
-      .setTitle('Ha eliminado un mensaje:')
-      .setDescription(message.content || '(No Content)')
-      .setFooter({ text: message.guild.name, iconURL: message.guild.iconURL() })
-      .setTimestamp(message.createdAt);
+module.exports = {  
+    execute: (message, authorData) => {
+        
+        const embed = new EmbedBuilder()
+            .setColor(authorData.color)
+            .setAuthor({
+                name: authorData.nombre + ' (' + "ID :" + authorData.id + ')',
+                iconURL: authorData.avatar,
+            })
+            .setTitle('Ha eliminado un mensaje :')
+            .setDescription(message.content|| '(No Content)') 
+            .setFooter({ text: message.guild.name , iconURL: message.guild.iconURL() })
+            .setTimestamp(message.createdAt)
+            ;
 
-    message.channel.send({ embeds: [embed] })
-      .then(() => console.log('✅ Embed de autosnipe enviado correctamente'))
-      .catch((error) => console.error('❌ Error al enviar embed de autosnipe:', error));
-  },
-};
+        message.channel.send({ embeds: [embed] });
+
+} 
+}
