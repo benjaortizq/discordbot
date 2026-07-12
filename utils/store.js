3e84bb87a9dbc89ea7f0d1cbcb27559b574fc38c
Conoutlet <benjaoq@gmail.com>
2026-07-12 12:32:00 -0600

    feat: add flag management functions for configuration handling

--- utils/store.js
+++ utils/store.js
@@ -38,6 +38,19 @@ function getMensajeEliminado(guildId, channelId) {
   return datos.mensajesEliminados?.[channelId] ?? null;
 }
 
+function tieneFlag(configActual, flag) {
+  return (configActual & flag) === flag; // AND b2b
+}
+
+function activarFlag(configActual, flag) {
+  return configActual | flag; // OR b2b
+}
+
+function desactivarFlag(configActual, flag) {
+  return configActual & ~flag; // i & !i 
+}
+
+
 module.exports = {
   guardarMensajeEliminado,
   getMensajeEliminado,
